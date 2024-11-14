6aae3732a4c980d38f108d0d1a8ed70e30521ade
lyleyyy <lyletwro@gmail.com>
2024-11-14 21:32:04 +0100

    new courses

---
+++ Section-10-11/src/app.ts
@@ -0,0 +1,8 @@
+import { ProjectInput } from "./components/project-input";
+import { ProjectList } from "./components/project-list";
+
+const prjInput = new ProjectInput();
+const activePrjList = new ProjectList("active");
+const finishedPrjList = new ProjectList("finished");
+
+console.log("waya");
